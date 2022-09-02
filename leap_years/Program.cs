6742da78c031788f43df37a4f043e1257fb30cca
Johnson Osita <80931376+Germaine-jay@users.noreply.github.com>
2022-09-02 03:25:56 +0100

    Add files via upload

---
+++ leap_years/Program.cs
@@ -0,0 +1,17 @@
+﻿var n = new List<int>();
+for(int i=0;i<=84;i++)
+{
+    int year = 2022+i;
+    int days = DateTime.DaysInMonth(year,2);
+
+    if(days == 29)
+    {
+        n.Add(year);
+    }
+}
+
+foreach(int r in n)
+{
+    Console.WriteLine($"The year {r} is a leap year");
+}
+Console.WriteLine(n.Count);
